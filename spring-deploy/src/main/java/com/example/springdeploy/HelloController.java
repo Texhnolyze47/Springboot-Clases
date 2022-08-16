534fb729d31ead8beee36855beba018315ccccba
Texhnolyze <ichigobankai136@gmail.com>
2022-08-16 16:56:44 -0500

    uso de variables de entorno

--- spring-deploy/src/main/java/com/example/springdeploy/HelloController.java
+++ spring-deploy/src/main/java/com/example/springdeploy/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${app.message}")
+    @Value("${app.varexample}")
     String message;
     @GetMapping("/hola")
     public String holaMundo(){
--- spring-deploy/src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
+++ spring-deploy/src/test/java/com/example/springdeploy/SpringDeployApplicationTests.java
@@ -3,11 +3,15 @@ package com.example.springdeploy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SpringDeployApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Un ejemplo de variables de entorno es JAVA_HOME
+		System.getenv().forEach(
+				(key,value) -> System.out.println(key + " " + value)
+		);
 	}
 
 }
