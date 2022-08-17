0916ef446290720b263380ceb19aaeeb94a1098e
Texhnolyze <ichigobankai136@gmail.com>
2022-08-17 17:33:35 -0500

    add configuracion de autenticacion basica

--- ob-spring-security/src/main/java/com/example/obspringsecurity/WebSecurityConfig.java
+++ ob-spring-security/src/main/java/com/example/obspringsecurity/WebSecurityConfig.java
@@ -2,15 +2,21 @@ package com.example.obspringsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
@@ -19,12 +25,41 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests((authz) -> authz
                         //con esto permite que la petición con /hola se pueda ver sin la autenticación
                         .antMatchers("/hola").permitAll()
+                        .antMatchers("/bootstrap").hasRole("ADMIN")
                         .anyRequest().authenticated()
         )
                 .httpBasic(withDefaults());
 
         return http.build();
     }
+
+    @Bean
+    public HttpFirewall httpFirewall(){
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowBackSlash(true);
+        firewall.setAllowSemicolon(true);
+        return  firewall;
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername("admin")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER", "ADMIN")
+                .build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
 
 
