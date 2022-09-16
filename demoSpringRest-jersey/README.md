3a621b2baf7373a486a529c8877dc6aaa27cf3dc
Texhnolyze <ichigobankai136@gmail.com>
2022-09-16 16:51:06 -0500

    se cancela

--- demoSpringRest-jersey/README.md
+++ demoSpringRest-jersey/README.md
@@ -4,3 +4,6 @@
 Los servicios conectan los controladores con el modelo, en otras palabras
 el servicio es el que manipula el modelo.
 
+## Injeccion de dependencias
+Cuando nosotros tenemos un serivcio el constructor debe tener ese
+parametro como servicio
--- demoSpringRest-jersey/src/main/java/com/example/demospringrestjersey/controllers/BootCampersController.java
+++ demoSpringRest-jersey/src/main/java/com/example/demospringrestjersey/controllers/BootCampersController.java
@@ -1,16 +1,30 @@
 package com.example.demospringrestjersey.controllers;
 
+import com.example.demospringrestjersey.models.Bootcamper;
+import com.example.demospringrestjersey.service.BootcamperService;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.List;
 
 @Component
 @Path("/")
 public class BootCampersController {
 
-    @GET
-    public String hola(){
-        return "Hola";
+    private BootcamperService bootcamperService;
+
+
+    public BootCampersController(BootcamperService bootcamperService){
+        this.bootcamperService = bootcamperService;
     }
+
+    @GET
+    @Path("/bootcampers")
+    @Produces("text/plain")
+    public List<Bootcamper> listarTodos(){
+        return bootcamperService.getAll();
+    }
+
 }
