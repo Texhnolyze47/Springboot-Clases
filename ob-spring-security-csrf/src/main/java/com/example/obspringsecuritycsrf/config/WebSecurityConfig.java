2ac3a57236dd29c475e324f9721e09fc01806fb8
Texhnolyze <ichigobankai136@gmail.com>
2022-09-16 16:29:34 -0500

    add: cors configuration

--- ob-spring-security-csrf/src/main/java/com/example/obspringsecuritycsrf/config/WebSecurityConfig.java
+++ ob-spring-security-csrf/src/main/java/com/example/obspringsecuritycsrf/config/WebSecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -42,6 +47,18 @@ public class WebSecurityConfig {
                 )
                 .httpBasic(withDefaults());
         return  http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:4200"));
+        configuration.setAllowedMethods(List.of("GET","POST","OPTIONS","DELETE", "PUT","PATCH"));
+        configuration.setAllowedHeaders(List.of("Access-Control-Allow-Origin", "X-Request-With", "Origin", "Content-Type", "Accept"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return  source;
     }
 
     /**
